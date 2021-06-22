e3f555ae0f12d5dac7c0cad61ab88ba916da5144
Hamza Usmani <37268659+hamzausmani302@users.noreply.github.com>
2021-06-22 14:02:21 +0000

    sendfile

--- myApp.js
+++ myApp.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var app = express();
 
+let path = __dirname + "/views/index.html";
+
+
 console.log("Hello World");
 
 
 app.get("/" , (req,res)=>{
-  res.send("Hello Express");
+  res.sendFile(path);
   
 
 })
