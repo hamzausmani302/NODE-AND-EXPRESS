a5da39c9f777be3a13d796f9eac9041d06e0aec1
Hamza Usmani <37268659+hamzausmani302@users.noreply.github.com>
2021-06-22 14:16:32 +0000

    css added

--- myApp.js
+++ myApp.js
@@ -2,9 +2,15 @@ var express = require('express');
 var app = express();
 
 let path = __dirname + "/views/index.html";
+let csspath = __dirname + "/public";
+;
+
+
 console.log(path);
 
 console.log("Hello World");
+
+app.use("/public" ,express.static(csspath) );
 
 
 app.get("/" , (req,res)=>{
